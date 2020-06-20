fbcf5a10c136f3ab619ceea1fa8f805d76efc8b2
uclone <email.lbc@gmail.com>
2020-06-20 12:15:00 +0900

    apt-gram

--- resident/models.py
+++ resident/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Resident(models.Model):
     dong = models.CharField(max_length=100, verbose_name = "동")
     ho = models.CharField(max_length=100, verbose_name = "호")
-    represent = models.CharField(max_length=100)
+    represent = models.CharField(max_length=100, verbose_name='입주민')
     family = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     date = models.DateField()
