9518171856350226205383d83e58c1df897be76e
uclone <email.lbc@gmail.com>
2020-06-21 16:27:56 +0900

    apt-gram

--- filegram/views.py
+++ filegram/views.py
@@ -14,10 +14,10 @@ from django.contrib.auth import get_user
 
 @login_required
 def file_list(request):
-    request.user = get_user(request)
-    grrr_id = request.user.groups.values_list('id', flat=True)        #.first()  # for "group_name" use 'name' instead of 'id'
-    gr_id = grrr_id[0]
-    pagefiles = File.objects.filter(group_id=gr_id)
+    #request.user = get_user(request)
+    #grrr_id = request.user.groups.values_list('id', flat=True)        #.first()  # for "group_name" use 'name' instead of 'id'
+    #gr_id = grrr_id[0]
+    pagefiles = File.objects.filter(group_id=1)
     #pagefiles = File.objects.filter(author=request.user)
     #pagination - start
     page = request.GET.get('page', 1)
@@ -33,8 +33,8 @@ def file_list(request):
 
 @login_required
 def file_search(request):
-    # user = auth.get_user(request)
-    gr_id = request.user.groups.values_list('id', flat=True).first()  # for "group_name" use 'name' instead of 'id'
+    xuser = get_user(request)
+    gr_id = xuser.groups.values_list('id', flat=True).first()  # for "group_name" use 'name' instead of 'id'
     file_list = File.objects.filter(group_id=gr_id)
     file_filter = SearchFilter(request.GET, queryset=file_list)
     x = file_filter.qs
