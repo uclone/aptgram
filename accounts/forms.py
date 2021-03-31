5c24542f636f9b3fab0a69273e97a0340a5e7413
uclone <email.lbc@gmail.com>
2021-03-31 12:03:14 +0900

    apt-gram

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.urls import reverse_lazy, reverse
 
+subject_choice= [
+    ('스마트가스기기', '스마트가스기기'),
+    ('업무용', '업무용'),
+    ]
+
 class RegisterForm(forms.ModelForm):
-    group = forms.ModelChoiceField(queryset=Group.objects.all(), label='사용목적', required=True)
+    group = forms.ModelChoiceField(queryset=Group.objects.all()[1:3], label='사용목적', required=True)
+    #group = forms.ModelChoiceField(queryset=Group.objects.order_by('-last_name')[:2], label='사용목적', required=True)
     email = forms.EmailField(label='이메일')
     last_name = forms.CharField(label='스마트폰번호: (-)은 생략', required=True)
     password = forms.CharField(label='패스워드(구글계정 패스워드와 동일해야 합니다.)', widget=forms.PasswordInput)
@@ -21,7 +27,7 @@ class RegisterForm(forms.ModelForm):
         return cd['password2']
 
 class UpdateForm(forms.ModelForm):
-    group = forms.ModelChoiceField(queryset=Group.objects.all(), label='사용목적', required=True)
+    group = forms.ModelChoiceField(queryset=Group.objects.all()[1:3], label='사용목적', required=True)
     email = forms.EmailField(label='이메일')
     last_name = forms.CharField(label='스마트폰번호: (-)은 생략', required=True)
 
